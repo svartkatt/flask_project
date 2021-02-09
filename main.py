94df6b81da65b587b6618661e7efd4c4b2d3c4cc
svartkatt <72262706+svartkatt@users.noreply.github.com>
2021-02-09 15:48:10 +0200

    Update main.py
    Промежуточный результат дз 4.

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from flask import Flask
 import requests
+import sqlite3
 
 app = Flask(__name__)
+with sqlite3.connect('server.db') as db:
+    cursor = db.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS exchange (
+    currency_to TEXT, 
+    exchange_rate BIGINT, 
+    amount BIGINT, 
+    result BIGINT
+)""")
+
+db.commit()
 
 
 def get_rates():
@@ -11,16 +23,17 @@ def get_rates():
     return rates
 
 
-def writing_to_history(currency, amount, result):
-    with open('history.py', 'a') as text:
-        text.write('"' + currency + '", ' + str(get_rates().get(currency)) + ', ' + str(amount) + ', ' + result + '\n')
+def writing_to_db(currency, amount, result):
+    cursor.execute("SELECT * FROM exchange")
+    cursor.execute("INSERT INTO exchange VALUES (?, ?, ?)", (currency, get_rates().get(currency), amount, result))
+    db.commit()
 
 
 @app.route('/eur_to_usd/<int:amount>/')
 def euro_to_usd(amount):
     currency = 'USD'
     result = str(get_rates().get(currency) * amount)
-    writing_to_history(currency, amount, result)
+    writing_to_db(currency, amount, result)
     return result
 
 
@@ -28,7 +41,7 @@ def euro_to_usd(amount):
 def euro_to_gbp(amount):
     currency = 'EUR'
     result = str(get_rates().get(currency) * amount)
-    writing_to_history(currency, amount, result)
+  #  writing_to_db(currency, amount, result)
     return result
 
 
@@ -36,7 +49,7 @@ def euro_to_gbp(amount):
 def euro_to_php(amount):
     currency = 'PHP'
     result = str(get_rates().get(currency) * amount)
-    writing_to_history(currency, amount, result)
+    writing_to_db(currency, amount, result)
     return result
 
 
